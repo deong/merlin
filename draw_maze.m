d08a7ad815bbaca2554680401cd59ef8dc802a92
EgillEinarss <blocknorad@gmail.com>
2012-08-31 15:45:05 +0000

    draw_maze.m updated.

--- draw_maze.m
+++ draw_maze.m
@@ -22,7 +22,14 @@
 %               and so on.
 % After: Only the side effect of displaying the maze
 %       on a per task basis as one image.
-function draw_maze(transition, reward, task, Q)
+function draw_maze(transition, reward, task, Q, mult)
+    if mult < 5, mult = 5; end
+    mhl = ceil(mult/2);
+    if mod(mult,2) == 0
+        mhr = mhl+1;
+    else
+        mhr = mhl;
+    end
     state = length(transition);
     transition = transition + 1;
     half = ceil(task./2);
@@ -34,28 +41,28 @@ function draw_maze(transition, reward, task, Q)
     end
     w = transition(a, 3) - a;
     h = state/w;
-    walls = zeros(h*5, w*5);
+    walls = zeros(h*mult, w*mult);
     
     for i = 0:w-1
         for j = 0:h-1
             if transition(j*w + i + 1, 1) == 0
-                walls(j*5 + 1, i*5 + 1: (i + 1)*5) = 1;
+                walls(j*mult + 1, i*mult + 1: (i + 1)*mult) = 1;
             end
             if transition(j*w + i + 1, 2) == 0
-                walls(j*5 + 1: (j + 1)*5,(i + 1)*5) = 1;
+                walls(j*mult + 1: (j + 1)*mult,(i + 1)*mult) = 1;
             end
             if transition(j*w + i + 1, 3) == 0
-                walls((j + 1)*5, i*5 + 1: (i + 1)*5) = 1;
+                walls((j + 1)*mult, i*mult + 1: (i + 1)*mult) = 1;
             end
             if transition(j*w + i + 1, 4) == 0
-                walls(j*5 + 1: (j + 1)*5, i*5 + 1) = 1;
+                walls(j*mult + 1: (j + 1)*mult, i*mult + 1) = 1;
             end
         end
     end
     
     figure(1); clf;
     for t = 1:task
-        maze = zeros(h*5, w*5);
+        maze = zeros(h*mult, w*mult);
         for i = 0: state-1
 %             c = 4;
 %             for j = 0:3
@@ -76,33 +83,51 @@ function draw_maze(transition, reward, task, Q)
         m = max(q)*1.33;
         for i = 0:w-1
             for j = 0:h-1
-                if reward(j*w + i + 1,t) == 0
-                    maze(j*5 + 1: (j + 1)*5, i*5 + 1: (i + 1)*5) = q(j*w + i + 1)/m;
-                    if d(j*w + i + 1) == 1
-                        maze(j*5 + 1, i*5 + 3) = 0;
-                    end
-                    if d(j*w + i + 1) == 2
-                        maze(j*5 + 3, (i + 1)*5) = 0;
-                    end
-                    if d(j*w + i + 1) == 3
-                        maze((j + 1)*5, i*5 + 3) = 0;
-                    end
-                    if d(j*w + i + 1) == 4
-                        maze(j*5 + 3, i*5 + 1) = 0;
-                    end
-                else
-                    maze(j*5 + 1: (j + 1)*5, i*5 + 1: (i + 1)*5) = ...
-                        [ 0 1 0 1 0
-                          1 0 1 0 1
-                          0 1 0 1 0
-                          1 0 1 0 1
-                          0 1 0 1 0 ];
+                maze(j*mult + 1: (j + 1)*mult, i*mult + 1: (i + 1)*mult)...
+                    = q(j*w + i + 1)/m;
+                if d(j*w + i + 1) == 1
+                    maze(j*mult + 1, i*mult + mhl: i*mult + mhr) = 0;
+                end
+                if d(j*w + i + 1) == 2
+                    maze(j*mult + mhl: j*mult + mhr, (i + 1)*mult) = 0;
+                end
+                if d(j*w + i + 1) == 3
+                    maze((j + 1)*mult, i*mult + mhl: i*mult + mhr) = 0;
+                end
+                if d(j*w + i + 1) == 4
+                    maze(j*mult + mhl: j*mult + mhr, i*mult + 1) = 0;
                 end
             end
         end
         maze = max(maze - walls, 0);
-        disp(sprintf('%d: %d repeated elements, min is %5.5f and max is %5.5f', ...
-            t, length(unique(maze)), min(min(maze)), max(max(maze))))
+        cflag = zeros(mult);
+        for i = 1:mult
+            if mod(i,2) == 0
+                for j = 1:mult
+                    if mod(j,2) == 0
+                        cflag(i,j) = 0;
+                    else
+                        cflag(i,j) = 1;
+                    end
+                end
+            else
+                for j = 1:mult
+                    if mod(j,2) == 0
+                        cflag(i,j) = 1;
+                    else
+                        cflag(i,j) = 0;
+                    end
+                end
+            end
+        end
+        for i = 0:w-1
+            for j = 0:h-1
+                if reward(j*w + i + 1,t) ~= 0
+                    maze(j*mult + 1: (j + 1)*mult, ...
+                        i*mult + 1: (i + 1)*mult) = cflag;
+                end
+            end
+        end
         subplot(2, half, t);
         imagesc(maze);
         t_s = 'task ';
@@ -110,6 +135,10 @@ function draw_maze(transition, reward, task, Q)
         title(t_s);
         axis image;
     end
-    %colormap hot;
+    colormap jet;
+    c = colormap;
+    c(1,1:3) = 0;
+    c(64,1:3) = 1;
+    colormap(c);
     shg
 end
