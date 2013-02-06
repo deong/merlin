eebe857810294aa2e9bfcd7dbab0be1155de53d6
Deon Garrett <deong@cataclysmicmutation.com>
2013-02-06 17:10:44 +0000

    added function to create mazes

--- mdpgen.py
+++ mdpgen.py
@@ -29,7 +29,6 @@ import numpy.random as npr
 import networkx as nx
 
 
-
 #
 # create random reward structure for an (nstates, nactions) MDP
 #
@@ -161,6 +160,79 @@ def write_instance(G, R):
         print(line)
 
 
+
+#
+# Multi-Maze Generator using Depth-first Search
+# Multi-Maze: Maze w/ multiple paths to solve
+#
+# Code adapted from 
+# http://code.activestate.com/recipes/578378-random-multi-maze-generator/
+# MIT License.
+#
+# The output of this function is a 2d matrix structure, where the (i,j) pair
+# is the "path number", i.e., maze[i][j] = k if cell (i,j) in the maze is
+# used by the k-th distinct path through the maze.
+# 
+def make_multimaze(width, height):
+    m = random.randint(5, 15) # of maze paths
+
+    # width and height of the maze
+    mx = width  
+    my = height 
+
+    # 4 directions to move in the maze
+    dx = [0, 1, 0, -1] 
+    dy = [-1, 0, 1, 0] 
+
+    maze = [[0 for x in range(mx)] for y in range(my)]
+
+    stack = [] # array of stacks
+    for i in range(m):
+        while True:
+            kx = random.randint(0, mx - 1); ky = random.randint(0, my - 1)
+            if maze[ky][kx] == 0: break
+        stack.append([(kx, ky)])
+        maze[ky][kx] = i + 1
+
+    cont = True # continue
+    while cont:
+        cont = False
+        for p in range(m):
+            if len(stack[p]) > 0:
+                cont = True # continue as long as there is a non-empty stack
+                (cx, cy) = stack[p][-1]
+                # find a new cell to add
+                nlst = [] # list of available neighbors
+                for i in range(4):
+                    nx = cx + dx[i]
+                    ny = cy + dy[i]
+                    if nx >= 0 and nx < mx and ny >= 0 and ny < my:
+                        if maze[ny][nx] == 0:
+                            # of occupied neighbors must be 1
+                            ctr = 0
+                            for j in range(4):
+                                ex = nx + dx[j]; ey = ny + dy[j]
+                                if ex >= 0 and ex < mx and ey >= 0 and ey < my:
+                                    if maze[ey][ex] == p + 1: ctr += 1
+                            if ctr == 1: nlst.append(i)
+                # if 1 or more neighbors available then randomly select one and add
+                if len(nlst) > 0:
+                    ir = nlst[random.randint(0, len(nlst) - 1)]
+                    cx += dx[ir]; cy += dy[ir]
+                    maze[cy][cx] = p + 1
+                    stack[p].append((cx, cy))
+                else: stack[p].pop()
+    return np.asarray(maze)
+
+
+#
+# take a maze created from make_multimaze and return an identical maze but
+# with only two "colors" corresponding to walls and open spaces
+# 
+def binarize_maze(maze):
+    return maze > 0
+
+
 if __name__ == '__main__':
     states = 100
     actions = 4
@@ -171,3 +243,7 @@ if __name__ == '__main__':
     D = randinst1(states, actions, R)
     G = randgraph1(states, actions)
     write_instance(G, D)
+
+    maze = make_maze(6, 6)
+    maze2 = make_multimaze(100,100)
+    print(maze)
